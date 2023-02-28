ac55f45acb359cfa6a4771863daabe146f9027b5
Matt Riemer <matthew@codeandeffect.com>
2023-02-28 15:32:13 -0700

    update calls

--- app/helpers/effective_mailchimp_helper.rb
+++ app/helpers/effective_mailchimp_helper.rb
@@ -1,12 +1,12 @@
 module EffectiveMailchimpHelper
 
-  def mailchimp_user_fields(form, sync: false)
+  def mailchimp_user_fields(form)
     raise('expected a form') unless form.respond_to?(:object)
 
     resource = form.object
     raise('expected an effective_mailchimp_user resource') unless resource.class.respond_to?(:effective_mailchimp_user?)
 
-    resource.mailchimp_sync!(force: sync)
+    resource.mailchimp_sync!(force: false)
 
     render('effective/mailchimp_user/fields', form: form, f: form, resource: resource, mailchimp_user: resource)
   end
